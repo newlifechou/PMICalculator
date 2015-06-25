1e228462886c6e8f02f47d2375b81461e665aabf
newlifechou <newlifechou@gmail.com>
2015-06-25 11:14:48 +0800

    往NewSheet窗口上添加提示

--- PMICostCalculator/NewSheet.Designer.cs
+++ PMICostCalculator/NewSheet.Designer.cs
@@ -33,27 +33,28 @@
             this.btnNew = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.txtRemark = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Location = new System.Drawing.Point(12, 61);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(134, 13);
+            this.label1.Size = new System.Drawing.Size(63, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "CostCalcuationSheetName";
+            this.label1.Text = "SheetName";
             // 
             // txtCostCalcualteName
             // 
-            this.txtCostCalcualteName.Location = new System.Drawing.Point(144, 10);
+            this.txtCostCalcualteName.Location = new System.Drawing.Point(73, 58);
             this.txtCostCalcualteName.Name = "txtCostCalcualteName";
-            this.txtCostCalcualteName.Size = new System.Drawing.Size(193, 20);
+            this.txtCostCalcualteName.Size = new System.Drawing.Size(264, 20);
             this.txtCostCalcualteName.TabIndex = 0;
             // 
             // btnNew
             // 
-            this.btnNew.Location = new System.Drawing.Point(12, 102);
+            this.btnNew.Location = new System.Drawing.Point(12, 150);
             this.btnNew.Name = "btnNew";
             this.btnNew.Size = new System.Drawing.Size(325, 43);
             this.btnNew.TabIndex = 2;
@@ -64,7 +65,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 32);
+            this.label2.Location = new System.Drawing.Point(12, 80);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(44, 13);
             this.label2.TabIndex = 0;
@@ -72,23 +73,34 @@
             // 
             // txtRemark
             // 
-            this.txtRemark.Location = new System.Drawing.Point(12, 48);
+            this.txtRemark.Location = new System.Drawing.Point(12, 96);
             this.txtRemark.Multiline = true;
             this.txtRemark.Name = "txtRemark";
             this.txtRemark.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.txtRemark.Size = new System.Drawing.Size(325, 48);
             this.txtRemark.TabIndex = 1;
             // 
+            // label3
+            // 
+            this.label3.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label3.Location = new System.Drawing.Point(12, 6);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(325, 46);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Recommendation:\r\nOrderDate+CompanyName+MaterialName+Other\r\n150625-Midsummer-CIGS-" +
+    "2.0";
+            // 
             // NewSheet
             // 
             this.AcceptButton = this.btnNew;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(349, 154);
+            this.ClientSize = new System.Drawing.Size(349, 204);
             this.Controls.Add(this.btnNew);
             this.Controls.Add(this.txtRemark);
             this.Controls.Add(this.txtCostCalcualteName);
             this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
             this.Name = "NewSheet";
             this.Text = "NewCostCalcuationSheet";
@@ -104,5 +116,6 @@
         private System.Windows.Forms.Button btnNew;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtRemark;
+        private System.Windows.Forms.Label label3;
     }
 }

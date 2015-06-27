7c2b3d29671bee5d7b4989f04c4a1764900c59cb
newlifechou <newlifechou@gmail.com>
2015-06-27 21:20:09 +0800

    更换图标

--- PMISpecificCalculator/MainForm.Designer.cs
+++ PMISpecificCalculator/MainForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
@@ -309,6 +310,7 @@
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox3);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
             this.Text = "专用计算器集合";
             this.groupBox1.ResumeLayout(false);

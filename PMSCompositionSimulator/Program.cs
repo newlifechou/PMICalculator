7a27a78fb67977da49fdf414e0aed0e67e1e2437
xs.zhou <xs.zhou@cdpmi.net>
2017-11-07 14:44:25 +0800

    use another way to do

--- PMSCompositionSimulator/Program.cs
+++ PMSCompositionSimulator/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
-
+using System.Diagnostics;
 
 namespace PMSCompositionSimulator
 {
@@ -15,78 +15,37 @@ namespace PMSCompositionSimulator
             Console.WriteLine("欢迎使用成分模拟生成器Version2.0，请按照提示继续操作");
             bool isQuit = false;
             string input = "";
-            while (!isQuit)
+
+            try
             {
-                bool isLast = false;
-                List<SingleElement> elements = new List<SingleElement>();
-
-                try
+                while (!isQuit)
                 {
-                    int recordCount = 5;
-                    Console.WriteLine("请输入需要的记录总数");
+                    string templateFile = Path.Combine(Environment.CurrentDirectory, "Template.txt");
+                    Console.WriteLine("需要改写模板吗(y/n)？");
                     input = Console.ReadLine();
-                    int.TryParse(input, out recordCount);
-
-                    while (!isLast)
+                    if (input=="y")
                     {
-                        Console.WriteLine("请输入名称并回车(字符)");
-                        input = Console.ReadLine();
-                        var tempElement = new SingleElement();
-                        tempElement.Element = input;
-
-                        Console.WriteLine("请输入比例并回车(数字)");
-                        input = Console.ReadLine();
-                        double at = 0;
-                        double.TryParse(input, out at);
-                        tempElement.Ratio = at;
-
-                        Console.WriteLine("请输入偏差(数字)(默认是0.5)");
-                        input = Console.ReadLine();
-                        double offset = 0.5;
-                        double.TryParse(input, out offset);
-                        tempElement.Offset = offset;
-
-                        elements.Add(tempElement);
-
-                        Console.WriteLine("结束请输入ok");
-                        isLast = (input == "ok");
+                        Console.WriteLine("请按照特定模式改写模板，保存后关闭回车继续");
+                        Process.Start(templateFile);
                     }
-                    //calculate it
-                    double allAt = elements.Sum(s => s.Ratio);
-                    elements.ForEach(i =>
-                    {
-                        i.Ratio = i.Ratio / allAt;
-                    });
+                    Console.WriteLine("模版改写完毕,回车继续");
+                    input=Console.ReadLine();
 
-                    string outputFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), DateTime.Now.ToString() + ".csv");
-                    StreamWriter sw = new StreamWriter(outputFile);
-                    sw.Write("No.");
-                    elements.ForEach(i => {
-                        sw.Write(",");
-                        sw.Write(i.Element);
-                    });
-                    sw.WriteLine();
-
-                    Random r = new Random();
-
-                    sw.Flush();
-                    sw.Close();
-
-                    Console.WriteLine("csv文件在桌面上");
-                }
-                catch (Exception)
-                {
-
-                }
-                finally
-                {
-                    elements.Clear();
+                    Console.WriteLine("(退出请输入exit)");
+                    input = Console.ReadLine();
+                    isQuit = (input == "exit");
                 }
 
-
-                Console.WriteLine("(退出请输入exit)");
-                isQuit = (input == "exit");
             }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                Console.WriteLine("ByeBye");
+            }
+
         }
     }
 }
